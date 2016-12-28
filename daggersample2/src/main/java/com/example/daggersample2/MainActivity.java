8dbe0a0b32d3570562096bdd478df6a0d82ec56b
Shaik, Umair Anjum (US - Mumbai) <umshaik@deloitte.com>
2016-12-28 14:38:12 +0530

    Intial commit

--- daggersample2/src/main/java/com/example/daggersample2/MainActivity.java
+++ daggersample2/src/main/java/com/example/daggersample2/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.daggersample2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.example.daggersample2.model.Vehicle;
 import com.example.daggersample2.dagger.DaggerVehicleComponent;
 import com.example.daggersample2.dagger.VehicleComponent;
 import com.example.daggersample2.dagger.VehicleModule;
+import com.example.daggersample2.model.Vehicle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         VehicleComponent vehicleComponent= DaggerVehicleComponent.builder().vehicleModule(new VehicleModule()).build();
 
         Vehicle vehicle = vehicleComponent.provideVehicle();
